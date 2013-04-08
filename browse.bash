79e580f725d7f8ad7e96171b3a9ec4e4604f6602
wenjun.yan <mylastnameisyan@gmail.com>
2013-04-08 23:12:40 +0900

    font-size.difference in firefox and chrome;

--- browse.bash
+++ browse.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 img2ascii() {
-    echo "<pre><span style=\"font-size:0.1em;letter-spacing:0;line-height:0.45;\">`python ~/code/img2ascii/img2ascii.py $@`</span></pre>" | browser
+    echo "<pre><span style=\"font-size:8px;letter-spacing:0;line-height:0.45;align:center;\">`python ~/code/img2ascii/img2ascii.py $@`</spn></pre>" | browser
 }
